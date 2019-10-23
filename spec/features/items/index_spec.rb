ae7034a05ac36c895a8fd6224974377c5fd42d11
Rachel Lew <rlew07@gmail.com>
2019-10-23 11:22:23 -0600

    Add tests and functionality to not display disabled items and to add links to item images
    Co-authored-by: Jonathan Patterson <47950816+Jonpatt92@users.noreply.github.com>
    Co-authored-by: Mary Lang <47950816+Jonpatt92@users.noreply.github.com>

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -19,8 +19,6 @@ RSpec.describe "Items Index Page" do
       expect(page).to have_link(@tire.merchant.name)
       expect(page).to have_link(@pull_toy.name)
       expect(page).to have_link(@pull_toy.merchant.name)
-      expect(page).to have_link(@dog_bone.name)
-      expect(page).to have_link(@dog_bone.merchant.name)
     end
 
     it "I can see a list of all of the items "do
@@ -31,7 +29,6 @@ RSpec.describe "Items Index Page" do
         expect(page).to have_link(@tire.name)
         expect(page).to have_content(@tire.description)
         expect(page).to have_content("Price: $#{@tire.price}")
-        expect(page).to have_content("Active")
         expect(page).to have_content("Inventory: #{@tire.inventory}")
         expect(page).to have_link(@meg.name)
         expect(page).to have_css("img[src*='#{@tire.image}']")
@@ -41,21 +38,62 @@ RSpec.describe "Items Index Page" do
         expect(page).to have_link(@pull_toy.name)
         expect(page).to have_content(@pull_toy.description)
         expect(page).to have_content("Price: $#{@pull_toy.price}")
-        expect(page).to have_content("Active")
         expect(page).to have_content("Inventory: #{@pull_toy.inventory}")
         expect(page).to have_link(@brian.name)
         expect(page).to have_css("img[src*='#{@pull_toy.image}']")
       end
 
-      within "#item-#{@dog_bone.id}" do
-        expect(page).to have_link(@dog_bone.name)
-        expect(page).to have_content(@dog_bone.description)
-        expect(page).to have_content("Price: $#{@dog_bone.price}")
-        expect(page).to have_content("Inactive")
-        expect(page).to have_content("Inventory: #{@dog_bone.inventory}")
-        expect(page).to have_link(@brian.name)
-        expect(page).to have_css("img[src*='#{@dog_bone.image}']")
+      expect(page).to_not have_link(@dog_bone.name)
+      expect(page).to_not have_content(@dog_bone.description)
+      expect(page).to_not have_content("Price: $#{@dog_bone.price}")
+      expect(page).to_not have_content("Inventory: #{@dog_bone.inventory}")
+      expect(page).to_not have_css("img[src*='#{@dog_bone.image}']")
+    end
+
+    it "I see all items in the system except disabled items" do
+
+      visit '/items'
+
+      within "#item-#{@tire.id}" do
+        expect(page).to have_link(@tire.name)
+        expect(page).to have_content(@tire.description)
+        expect(page).to have_content("Price: $#{@tire.price}")
+        expect(page).to have_content("Inventory: #{@tire.inventory}")
+        expect(page).to have_link(@meg.name)
+        expect(page).to have_css("img[src*='#{@tire.image}']")
       end
+
+      within "#item-#{@pull_toy.id}" do
+        expect(page).to have_link(@pull_toy.name)
+        expect(page).to have_content(@pull_toy.description)
+        expect(page).to have_content("Price: $#{@pull_toy.price}")
+        expect(page).to have_content("Inventory: #{@pull_toy.inventory}")
+        expect(page).to have_link(@brian.name)
+        expect(page).to have_css("img[src*='#{@pull_toy.image}']")
+      end
+
+      expect(page).to_not have_link(@dog_bone.name)
+      expect(page).to_not have_content(@dog_bone.description)
+      expect(page).to_not have_content("Price: $#{@dog_bone.price}")
+      expect(page).to_not have_content("Inventory: #{@dog_bone.inventory}")
+      expect(page).to_not have_css("img[src*='#{@dog_bone.image}']")
+    end
+
+    it "I can access each item's show page by clicking on the item's image on the items index page" do
+
+      visit '/items'
+
+      within "#item-#{@tire.id}" do
+        click_link "image-#{@tire.id}"
+      end
+      expect(current_path).to eq("/items/#{@tire.id}")
+
+      visit '/items'
+
+      within "#item-#{@pull_toy.id}" do
+        click_link "image-#{@pull_toy.id}"
+      end
+      expect(current_path).to eq("/items/#{@pull_toy.id}")
     end
   end
 end
