1455a16989a217adfff5bfa5dff815b89f661f7b
Jon Patterson <JonPatt92@gmail.com>
2019-10-23 18:48:13 -0600

    Commented out unfinished test

--- spec/features/merchant_admin/dashboard_spec.rb
+++ spec/features/merchant_admin/dashboard_spec.rb
@@ -15,12 +15,12 @@ describe "When I visit my dashboard as a Merchant" do
     expect(page).to have_content('65041')
   end
 
-  describe "Shows me a list of pending orders with my items"
-    it "Has Order id (links to order show), date of order, quantity of my items, total value of my items" do
-      pull_toy = @merchant_user.merchant.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
-      dog_bone = @merchant_user.merchant.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
-      user = User.create(name: 'Patti', address: '953 Sunshine Ave', city: 'Honolulu', state: 'Hawaii', zip: '96701', email: 'pattimonkey34@gmail.com', password: 'banana')
-
-
-    end
+  # describe "Shows me a list of pending orders with my items"
+  #   it "Has Order id (links to order show), date of order, quantity of my items, total value of my items" do
+  #     pull_toy = @merchant_user.merchant.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+  #     dog_bone = @merchant_user.merchant.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
+  #     user = User.create(name: 'Patti', address: '953 Sunshine Ave', city: 'Honolulu', state: 'Hawaii', zip: '96701', email: 'pattimonkey34@gmail.com', password: 'banana')
+  #
+  #
+  #   end
 end
