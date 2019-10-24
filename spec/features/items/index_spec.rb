5b71250cd1f6a8c8e3799700f4dfac33177da909
Rachel Lew <rlew07@gmail.com>
2019-10-24 11:44:12 -0600

    Adds feature and model tests for top five and bottom five items methods

--- spec/features/items/index_spec.rb
+++ spec/features/items/index_spec.rb
@@ -7,8 +7,17 @@ RSpec.describe "Items Index Page" do
       @brian = Merchant.create(name: "Brian's Dog Shop", address: '125 Doggo St.', city: 'Denver', state: 'CO', zip: 80210)
 
       @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      @shifter = @meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 20)
 
       @pull_toy = @brian.items.create(name: "Pull Toy", description: "Great pull toy!", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 32)
+      @squeaky_toy = @brian.items.create(name: "Squeaky Toy", description: "Awesome squeaky toy!", price: 15, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 30)
+      @dog_treats = @brian.items.create(name: "Dog Treats", description: "Pretty good dog treats. My dog liked them.", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 25)
+
+      @frisbee = @brian.items.create(name: "Frisbee", description: "Fido and I love playing with this frisbee at the park.", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 28)
+      @dog_bed = @brian.items.create(name: "Dog Bed", description: "Fido and I love playing with this frisbee at the park.", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 28)
+      @tennis_ball = @brian.items.create(name: "Tennis Ball", description: "Fido and I love playing with this frisbee at the park.", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 28)
+      @dog_shampoo = @brian.items.create(name: "Dog Shampoo", description: "Fido and I love playing with this frisbee at the park.", price: 10, image: "http://lovencaretoys.com/image/cache/dog/tug-toy-dog-pull-9010_2-800x800.jpg", inventory: 28)
+
       @dog_bone = @brian.items.create(name: "Dog Bone", description: "They'll love it!", price: 21, image: "https://img.chewy.com/is/image/catalog/54226_MAIN._AC_SL1500_V1534449573_.jpg", active?:false, inventory: 21)
     end
 
@@ -77,6 +86,62 @@ RSpec.describe "Items Index Page" do
       expect(page).to_not have_content("Price: $#{@dog_bone.price}")
       expect(page).to_not have_content("Inventory: #{@dog_bone.inventory}")
       expect(page).to_not have_css("img[src*='#{@dog_bone.image}']")
+    end
+
+    it "I can see an area with statistics that displays top 5 most popular items and bottom 5 least popular items" do
+      order_1 = Order.create!(name: 'Meg', address: '123 Stang Ave', city: 'Hershey', state: 'PA', zip: 17033)
+      order_2 = Order.create!(name: 'Brian', address: '123 Zanti St', city: 'Denver', state: 'CO', zip: 80204)
+      order_3 = Order.create!(name: 'Mike', address: '123 Dao St', city: 'Denver', state: 'CO', zip: 80210)
+
+      order_1.item_orders.create!(item: @tire, price: @tire.price, quantity: 2)
+      order_1.item_orders.create!(item: @tire, price: @tire.price, quantity: 2)
+      order_1.item_orders.create!(item: @tire, price: @tire.price, quantity: 2)
+      order_1.item_orders.create!(item: @pull_toy, price: @pull_toy.price, quantity: 20)
+      order_2.item_orders.create!(item: @shifter, price: @shifter.price, quantity: 18)
+      order_2.item_orders.create!(item: @squeaky_toy, price: @squeaky_toy.price, quantity: 19)
+      order_3.item_orders.create!(item: @dog_treats, price: @dog_treats.price, quantity: 15)
+      order_3.item_orders.create!(item: @frisbee, price: @frisbee.price, quantity: 5)
+      order_3.item_orders.create!(item: @dog_bed, price: @dog_bed.price, quantity: 1)
+      order_3.item_orders.create!(item: @tennis_ball, price: @tennis_ball.price, quantity: 2)
+      order_3.item_orders.create!(item: @dog_shampoo, price: @dog_shampoo.price, quantity: 3)
+
+      visit '/items'
+
+      within ".item-stats" do
+        within ".top-five-items" do
+          expect(page).to have_content(@pull_toy.name)
+          expect(page).to have_content("Quantity bought: 20")
+
+          expect(page).to have_content(@squeaky_toy.name)
+          expect(page).to have_content("Quantity bought: 19")
+
+          expect(page).to have_content(@shifter.name)
+          expect(page).to have_content("Quantity bought: 18")
+
+          expect(page).to have_content(@dog_treats.name)
+          expect(page).to have_content("Quantity bought: 15")
+
+          expect(page).to have_content(@tire.name)
+          expect(page).to have_content("Quantity bought: 6")
+
+          expect(page).to_not have_content(@frisbee.name)
+        end
+
+        within ".bottom-five-items" do
+          expect(page).to have_content(@frisbee.name)
+          expect(page).to have_content("Quantity bought: 5")
+
+          expect(page).to have_content(@dog_bed.name)
+          expect(page).to have_content("Quantity bought: 1")
+          expect(page).to have_content(@tennis_ball.name)
+          expect(page).to have_content("Quantity bought: 2")
+          expect(page).to have_content(@dog_shampoo.name)
+          expect(page).to have_content("Quantity bought: 3")
+          expect(page).to have_content(@tire.name)
+          expect(page).to have_content("Quantity bought: 6")
+          expect(page).to_not have_content(@shifter.name)
+        end
+      end
     end
 
     it "I can access each item's show page by clicking on the item's image on the items index page" do
