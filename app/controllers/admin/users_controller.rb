5879784af20590b01a8882d6818e994fae7adb2c
Rachel Lew <rlew07@gmail.com>
2019-10-26 13:03:46 -0600

    Adds update method to admin users controller
    Co-authored-by: Jonathan Patterson 
    <47950816+Jonpatt92@users.noreply.github.com>
    Co-authored-by: Mary Lang <47950816+Jonpatt92@users.noreply.github.com>

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,10 +2,26 @@ class Admin::UsersController < Admin::BaseController
   def edit
     @user = User.find(params[:user_id])
 
-    if request.env['PATH_INFO'] == "/profile/#{@user.id}/edit/password"
+    if request.env['PATH_INFO'] == "/admin/users/#{@user.id}/edit/password"
       @password_change = true
     else
       @password_change = false
+    end
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @user.update(user_params)
+
+    if @user.save
+      flash[:sucess] = "You have successfully updated #{@user.name}'s profile!" if request.env['REQUEST_METHOD'] == "PUT"
+      flash[:sucess] = "You have successfully updated #{@user.name}'s password!" if request.env['REQUEST_METHOD'] == "PATCH"
+      redirect_to "/admin/users"
+    else
+      flash[:error] = @user.errors.full_messages.to_sentence
+
+      redirect_to "/admin/users/#{@user.id}/edit" if request.env['REQUEST_METHOD'] == "PUT"
+      redirect_to "/admin/users/#{@user.id}/edit/password" if request.env['REQUEST_METHOD'] == "PATCH"
     end
   end
 
