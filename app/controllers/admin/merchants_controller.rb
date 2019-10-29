91a41feedbeec5df2916e4c005adf7107e0b33fc
Jon Patterson <JonPatt92@gmail.com>
2019-10-29 10:06:19 -0600

    Move merchant database changes to the merchant model, build methods for enable, disable, and merchant index

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -6,17 +6,31 @@ class Admin::MerchantsController < Admin::BaseController
     @orders = Order.find(order_ids)
   end
 
+  def index
+    @merchants = Merchant.all
+  end
+
+  def enable
+    merchant = Merchant.find(params[:id])
+    merchant.enable
+  end
+
+  def disable
+    merchant = Merchant.find(params[:id])
+    merchant.disable
+  end
+
   # !!Move into the Model!!
   def update_status
     merchant = Merchant.find(params[:merchant_id])
-    if merchant.status == 'disabled'
-      merchant.update_column(:status, 'enabled')
+    if merchant.enabled? == false
+      merchant.enable
       merchant.items.each do |item|
         item.update_column(:active?, true)
       end
       flash[:success] = "#{merchant.name} is now enabled."
-    elsif merchant.status == 'enabled'
-      merchant.update_column(:status, 'disabled')
+    elsif merchant.enabled?
+      merchant.disable
       merchant.items.each do |item|
         item.update_column(:active?, false)
       end
