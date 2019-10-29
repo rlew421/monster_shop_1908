7fb1b5b56e0a59663cbe51bcb8e17a97efa3f9c2
Jon Patterson <JonPatt92@gmail.com>
2019-10-29 11:36:38 -0600

    Implement Item Model methods to change state

--- app/controllers/admin/merchants_controller.rb
+++ app/controllers/admin/merchants_controller.rb
@@ -6,38 +6,21 @@ class Admin::MerchantsController < Admin::BaseController
     @orders = Order.find(order_ids)
   end
 
-  def index
-    @merchants = Merchant.all
-  end
-
-  def enable
-    merchant = Merchant.find(params[:id])
-    merchant.enable
-  end
-
-  def disable
-    merchant = Merchant.find(params[:id])
-    merchant.disable
-  end
-
-  # !!Move into the Model!!
   def update_status
     merchant = Merchant.find(params[:merchant_id])
-    if merchant.enabled? == false
+    if !merchant.enabled?
       merchant.enable
       merchant.items.each do |item|
-        item.update_column(:active?, true)
+        item.activate
       end
       flash[:success] = "#{merchant.name} is now enabled."
     elsif merchant.enabled?
       merchant.disable
       merchant.items.each do |item|
-        item.update_column(:active?, false)
+        item.deactivate
       end
       flash[:success] = "#{merchant.name} is now disabled."
     end
     redirect_to '/merchants'
   end
-
-
 end
