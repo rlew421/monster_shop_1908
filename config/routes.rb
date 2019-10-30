5facddc7224a780adb6709fc0644b5ff0dbe17f8
Jon Patterson <JonPatt92@gmail.com>
2019-10-30 14:12:45 -0600

    Delete redundant routes and organize

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :items, only: [:index, :new, :create]
   end
 
-  resources :items, except: [:new] do 
+  resources :items, except: [:new] do
     resources :reviews, only: [:new, :create]
   end
 
@@ -19,10 +19,10 @@ Rails.application.routes.draw do
   delete "/cart/:item_id", to: "cart#remove_item"
   patch "/cart/:item_id/:increment_decrement", to: "cart#increment_decrement"
 
-  delete '/cancel/:order_id', to: 'orders#destroy'
 
   get '/profile/orders', to: 'orders#index'
   get '/profile/orders/:order_id', to: 'orders#show'
+  delete '/cancel/:order_id', to: 'orders#destroy'
 
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
@@ -37,20 +37,18 @@ Rails.application.routes.draw do
   patch '/profile/:user_id/password', to: 'users#update'
 
   namespace :admin do
-    get '/orders/:order_id/ship', to: 'orders#ship'
-    get '/users/:user_id', to: 'users#show'
     get '/users', to: 'users#index'
     get '/users/:user_id', to: 'users#show'
     get '/users/:user_id/edit', to: 'users#edit'
     get '/users/:user_id/edit/password', to: 'users#edit'
     put '/users/:user_id', to: 'users#update'
     patch '/users/:user_id/password', to: 'users#update'
-    get '/:admin_id', to: 'dashboard#show'
     get '/users/:user_id/edit_role', to: 'users#edit_role'
     patch '/users/:user_id/upgrade', to: 'users#upgrade'
-    get '/', to: 'dashboard#show'
     get '/merchants/:merchant_id', to: 'merchants#show'
     patch '/merchants/:merchant_id/update_status', to: 'merchants#update_status'
+    get '/orders/:order_id/ship', to: 'orders#ship'
+    get '/', to: 'dashboard#show'
   end
 
   scope :admin do
@@ -60,17 +58,14 @@ Rails.application.routes.draw do
   namespace :merchant do
     get '/', to: 'dashboard#show'
     get '/items', to: 'items#index'
-    get '/items/:item_id/deactivate', to: 'items#update_status'
-    get '/items/:item_id/activate', to: 'items#update_status'
-    delete '/items/:item_id', to: 'items#destroy'
     get '/items/new', to: 'items#new'
     post '/items', to: 'items#create'
-
     get '/items/:item_id/edit', to: 'items#edit'
     patch '/items/:item_id', to: 'items#update'
-
+    delete '/items/:item_id', to: 'items#destroy'
+    get '/items/:item_id/deactivate', to: 'items#update_status'
+    get '/items/:item_id/activate', to: 'items#update_status'
     get '/orders/:order_id', to: 'orders#show'
     get '/item_orders/:item_order_id/fulfill', to: 'item_orders#fulfill'
-
   end
 end
