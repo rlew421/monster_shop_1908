bd515233e92b7b568f5096ec8e1ad7e59434a340
Jon Patterson <JonPatt92@gmail.com>
2019-10-30 14:13:43 -0600

    Change test to recognize only Merchant users can perform functionality

--- spec/features/merchants/edit_spec.rb
+++ spec/features/merchants/edit_spec.rb
@@ -1,12 +1,26 @@
 require 'rails_helper'
 
-RSpec.describe "As a Visitor" do
+RSpec.describe "As a Merchant User" do
   describe "After visiting a merchants show page and clicking on updating that merchant" do
     before :each do
       @bike_shop = Merchant.create(name: "Brian's Bike Shop", address: '123 Bike Rd.', city: 'Richmond', state: 'VA', zip: 11234)
+      @merchant_admin = @bike_shop.users.create(name: 'Monica', address: '75 Chef Ave', city: 'Utica', state: 'New York', zip: '45827', email: 'cleaner@gmail.com', password: 'monmon', role: 2)
+      @user_1 = User.create(name: 'Richy Rich', address: '102 Main St', city: 'NY', state: 'New York', zip: '10221', email: "young_money99@gmail.com", password: "momoneymoprobz")
+      @admin = User.create(name: 'Monica', address: '75 Chef Ave', city: 'Utica', state: 'New York', zip: '45827', email: 'cleaner@gmail.com', password: 'monmon', role: 3)
+
+      visit '/'
+      click_link 'Login'
+      fill_in :email, with: @merchant_admin.email
+      fill_in :password, with: @merchant_admin.password
+      click_button 'Log In'
+      click_link 'All Merchants'
     end
-    it 'I can see prepopulated info on that user in the edit form' do
-      visit "/merchants/#{@bike_shop.id}"
+
+    it 'I can see prepopulated info on that merchant in the edit form' do
+      click_link "#{@bike_shop.name}"
+      expect(current_path).to eq("/merchants/#{@bike_shop.id}")
+      expect(page).to_not have_link('Delete Merchant')
+
       click_on "Update Merchant"
 
       expect(page).to have_link(@bike_shop.name)
@@ -40,7 +54,7 @@ RSpec.describe "As a Visitor" do
 
       fill_in 'Name', with: ""
       fill_in 'Address', with: "1234 New Bike Rd."
-      fill_in 'City', with: ""
+      fill_in 'City', with: " "
       fill_in 'State', with: "CO"
       fill_in 'Zip', with: 80204
 
@@ -48,6 +62,21 @@ RSpec.describe "As a Visitor" do
 
       expect(page).to have_content("Name can't be blank and City can't be blank")
       expect(page).to have_button("Update Merchant")
+    end
+
+    it "Won't let regular users access edit or delete ability" do
+      click_on 'Log Out'
+      visit '/'
+      click_link 'Login'
+      fill_in :email, with: @user_1.email
+      fill_in :password, with: @user_1.password
+      click_button 'Log In'
+      click_link 'All Merchants'
+
+      click_link "#{@bike_shop.name}"
+      expect(current_path).to eq("/merchants/#{@bike_shop.id}")
+      expect(page).to_not have_link('Update Merchant')
+      expect(page).to_not have_link('Delete Merchant')
     end
   end
 end
